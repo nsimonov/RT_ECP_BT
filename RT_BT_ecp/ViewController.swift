8fda28a2ea9b4bf913da897b4fef71365c5ad0a6
Nikolay Simonov <nsimonov@MacBook-Air-2.local>
2015-05-23 12:28:50 +0300

    Initial commit from local git

---
+++ RT_BT_ecp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RT_BT_ecp
+//
+//  Created by Nikolay Simonov on 23.05.15.
+//  Copyright (c) 2015 Simon inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
